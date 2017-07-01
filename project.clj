bea478f4fa949ff1ba3cccb52b62bf945f2111c3
Ivanov Dmitry <errfrom@yandex.ru>
2017-07-01 16:08:08 +0300

    project conf described

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject vkmanager "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Structuring, processing and filtering public VK data."
+  :url "https://bitbucket.org/errfrom/vk-manager"
+  :license {:name "MIT License"
+            :url "https://choosealicense.com/licenses/mit/"}
   :dependencies [[org.clojure/clojure    "1.8.0"]
                  [proto-repl             "0.3.1"]      ; repl
                  [org.clojure/tools.cli  "0.3.5"]      ; console args parser
