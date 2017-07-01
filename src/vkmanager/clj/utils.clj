3700a7a23e8111c5265566d2d5dc837dd8881ff6
Ivanov Dmitry <errfrom@yandex.ru>
2017-07-02 01:12:43 +0300

    some adjuvant functions and tests for them were added

--- src/vkmanager/clj/utils.clj
+++ src/vkmanager/clj/utils.clj
@@ -5,3 +5,40 @@
   (-> text (#(str/split % #" |\n"))
            (#(filter (comp not nil? seq) %))
            (#(str/join delimiter %))))
+
+(def join-by-newline
+  (partial str/join "\n"))
+
+(defn align-by [delimiter & strings] ; TODO: не проходит тесты!
+  "Выравнивает строки по ограничителю."
+    (let [space   " "
+          pattern (re-pattern (format "[%s]" delimiter))
+          get-index #(str/last-index-of % delimiter)
+          max-index  (apply max
+                       (map (partial get-index)
+                             strings))
+
+          add-spaces #(str/join
+                        (str (str/join (repeat (- max-index (get-index %))
+                                               space))
+                              delimiter)
+                        (str/split % pattern))]
+    (vec (map (partial add-spaces) strings))))
+
+(defn flatten1 [x]
+  "Удаляет один уровень вложенности.
+   > flatten1 [a1 a2 [a3 a4]]
+   [a1 a2 a3 a4]"
+  (if (nil? (seq x)) []
+    (let [head (first x)
+          tail (rest  x)]
+      (if (sequential? head)
+        ; рекурсивно раскладывается двумя путями
+        ; если мы имеем [a1 a2 [a3 a4] a5]
+        ; получим  (cons a1
+        ;            (cons a2
+        ;              (into [a3 a4]
+        ;                (cons a5 [])
+        ; что сворачивается в [a1 a2 a3 a4 a5]
+        (into head (flatten1 tail))
+        (cons head (flatten1 tail))))))
--- test/vkmanager/clj/utils_test.clj
+++ test/vkmanager/clj/utils_test.clj
@@ -1,3 +1,20 @@
 (ns vkmanager.clj.utils_test
   (:require [clojure.test        :refer :all]
             [vkmanager.clj.utils :refer :all]))
+
+(deftest test-flatten1
+  (let [test1 [1 2 3 4]
+        test2 ["a" "b" 1 2]]
+    (is (= test1 (flatten1 [1 2 [3 4]])))
+    (is (= test1 (flatten1 [[1 2] 3 4])))
+    (is (= test1 (flatten1 [1 [2 3] 4])))
+    (is (= test2 (flatten1 ["a" "b" [1 2]])))))
+
+(deftest test-align-by ; TODO: 1 failure
+  (let [str1 "something | stuff"
+        str2 "anything | stuff"
+        test1 "something | stuff"
+        test2 "anything  | stuff"
+        delimiter "|"]
+    (is (= (quote (test1 test2))
+           (align-by delimiter str1 str2)))))
