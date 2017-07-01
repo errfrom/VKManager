3da4297ef7cfb175fa38e8ce921be8dcdf274c49
Ivanov Dmitry <errfrom@yandex.ru>
2017-07-01 17:29:42 +0300

    little fixes

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
                  [proto-repl             "0.3.1"]      ; repl
                  [org.clojure/tools.cli  "0.3.5"]      ; console args parser
                  [org.xerial/sqlite-jdbc "3.7.15-M1"]] ; jdbc driver
-  :main vkmanager.sql.core
-  :aot [vkmanager.sql.core]
+  :main vkmanager.clj.core
+  :aot [vkmanager.clj.core]
   :target-path "target/%s")
