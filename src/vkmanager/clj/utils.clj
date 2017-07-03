0deb8b601e66a1b190020386652748a459df93fb
Ivanov Dmitry <errfrom@yandex.ru>
2017-07-03 22:25:54 +0300

    some new functions were added

--- src/vkmanager/clj/utils.clj
+++ src/vkmanager/clj/utils.clj
@@ -1,5 +1,6 @@
 (ns vkmanager.clj.utils
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set    :refer [difference]]))
 
 (defn normalize-text [text & {:keys [delimiter] :or {delimiter ""}}]
   (-> text (#(str/split % #" |\n"))
@@ -27,7 +28,7 @@
 
 (defn flatten1 [x]
   "Удаляет один уровень вложенности.
-   > flatten1 [a1 a2 [a3 a4]]
+   > (flatten1 [a1 a2 [a3 a4]])
    [a1 a2 a3 a4]"
   (if (nil? (seq x)) []
     (let [head (first x)
@@ -49,6 +50,31 @@
     (str/join "," x)
     x))
 
-(defn rstrip [x]
+(defn lstrip [x]
   "Удаляет первый элемент строки."
   (str/join "" (rest x)))
+
+(defn add-missing-keys [x pattern]
+  "Сравнивает hashmap с шаблоном (последовательность ключей).
+   И, если в hashmap'е отсутствуют некоторые ключи из шаблона,
+   добавляет их со значением nil.
+   > (add-missing-keys {:a 1 :b 2} #(:a :b :c :d)
+   {:a 1 :b 2 :c nil :d nil}."
+  (let [excepted     (if (instance? java.util.Set pattern) pattern (set pattern))
+        existing     (set (keys x))
+        missing      (difference excepted existing)
+        missing-nils (apply merge (map #(hash-map % nil) missing))]
+    (merge x missing-nils)))
+
+(defn positive-integer? [x]
+  "Определяет, является ли число целым и положительным."
+  (re-matches #"^[1-9][0-9]*$" x))
+
+(defn update-if-contains [x key' fun]
+  "Обертка вокруг встроенной функции update.
+   Отличие заключается в обработке исключительной ситуации
+   при передаче несуществующего в ассоциативном массиве ключа:
+   обычная функция добавляет ключ, эта же возвращает первоначальный
+   hashmap"
+  (if (contains? x key') (update x key' fun)
+                         x))
