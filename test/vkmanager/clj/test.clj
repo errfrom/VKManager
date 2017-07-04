60cd76b22880db13dfd41d5af148b2f1ad64c3b7
Ivanov Dmitry <errfrom@yandex.ru>
2017-07-05 00:20:23 +0300

    'utils' tests-coveraged & some syntactic sugar for tests added

--- test/vkmanager/clj/test.clj
+++ test/vkmanager/clj/test.clj
@@ -3,8 +3,31 @@
 
 (defrecord Test [input output])
 
+(defn test [input output & {:keys [one] :or {one false}}]
+  "':one true' подразумевает передачу функции единственного
+   аргумента. Это всего лишь синтаксический сахар, чтобы не обрамлять
+   квадратными скобками input, если аргумент единственнен."
+  (if one (Test. [input] output)
+          (Test.  input  output)))
+
 (defn run [fun test']
-  (= (apply fun (.input test')) (.output test')))
+  (let [input  (.input test')
+        output (.output test')
+
+        ; equiv выбирает операции на основе
+        ; принадлежности выходного значения
+        ; числовому типу, т.к. = проверяет
+        ; хеш объектов, который для одинаковых
+        ; чисел может быть различным
+        equiv? (if (number? output) == =)]
+  (equiv? (apply fun input) output)))
 
 (defn run-all [fun tests]
-  (is (every? true? (map (partial run fun) tests))))
+  (let [result (is (every? true? (map (partial run fun) tests)))]
+    (when (not result)
+      (println (str "Тест функции " fun ".")))))
+
+(defmacro def-test [fun tests]
+  (let [test-name (symbol (str "test" (hash fun)))]
+    `(deftest ~test-name
+       (run-all ~fun ~tests))))
--- test/vkmanager/clj/utils_test.clj
+++ test/vkmanager/clj/utils_test.clj
@@ -1,41 +1,68 @@
 (ns vkmanager.clj.utils_test
   (:require [vkmanager.clj.test  :refer :all]
-            [clojure.test        :refer :all]
             [vkmanager.clj.utils :refer :all])
   (:import  [vkmanager.clj.test Test]))
 
-(deftest test-flatten1
-  (let [tests [(Test.
-                 [ [1 2 [3 4]] ]
-                 [1 2 3 4])
-               (Test.
-                 [ [[1 2] 3 4] ]
-                 [1 2 3 4])
-               (Test.
-                 [ [1 [2 3] 4] ]
-                 [1 2 3 4])
-               (Test.
-                 [ ["a" "b" [1 2]] ]
-                 ["a" "b" 1 2])]]
-  (run-all flatten1 tests)))
+(def-test flatten1
+  [(test [1 2 [3 4]]     [1 2 3 4]     :one true)
+   (test [[1 2] 3 4]     [1 2 3 4]     :one true)
+   (test [1 [2 3] 4]     [1 2 3 4]     :one true)
+   (test ["a" "b" [1 2]] ["a" "b" 1 2] :one true)])
 
-(deftest test-add-missing-keys
-  (let [tests [(Test. ; нормальный случай
-                 [{:a 1 :b 2} #{:c :d}]
-                 {:a 1 :b 2 :c nil :d nil})
-               (Test. ; другой тип шаблона
-                 [{:a 1 :b 2} [:c :d]]
-                 {:a 1 :b 2 :c nil :d nil})
-               (Test. ; пустой шаблон
-                 [{:a 1 :b 2} #{}]
-                 {:a 1 :b 2})
-               (Test. ; совпадение с шаблоном
-                 [{:a 1 :b 2} #{:a :b}]
-                 {:a 1 :b 2})
-               (Test. ; недостаточный шаблон
-                 [{:a 1 :b 2 :c 3} #{:a :b}]
-                 {:a 1 :b 2 :c 3})
-               (Test. ; все пустое
-                 [{} #{}]
-                 {}) ]]
-    (run-all add-missing-keys tests)))
+(def-test add-missing-keys
+  [(test [{:a 1 :b 2} #{:c :d}]      {:a 1 :b 2 :c nil :d nil})
+   (test [{:a 1 :b 2} [:c :d]]       {:a 1 :b 2 :c nil :d nil})
+   (test [{:a 1 :b 2} #{}]           {:a 1 :b 2})
+   (test [{:a 1 :b 2} #{:a :b}]      {:a 1 :b 2})
+   (test [{:a 1 :b 2 :c 3} #{:a :b}] {:a 1 :b 2 :c 3})
+   (test [{} #{}]                    {}) ])
+
+(def-test lstrip
+  [(test "-test" "test" :one true)
+   (test ""      ""     :one true)
+   (test "a"     ""     :one true)])
+
+(def-test positive-integer?
+  [(test "0"  false :one true)
+   (test "1"  true  :one true)
+   (test "-1" false :one true)])
+
+(def-test update-if-contains
+  [(test [{:a 1 :b 2 :c 3} :a str] {:a "1" :b 2 :c 3})
+   (test [{:a 1 :b 2 :c 3} :d str] {:a 1 :b 2 :c 3})
+   (test [{} :a str]               {})])
+
+(def-test remove-tenth
+  [(test 10   9 :one true)
+   (test 0    0 :one true)
+   (test -10 -9 :one true)
+   (test 11   9 :one true)])
+
+(def-test separate-by-commas
+  [(test [1 2 3]       "1,2,3" :one true)
+   (test ["a" "b" "c"] "a,b,c" :one true)
+   (test []            ""      :one true)
+   (test [1 "a"]       "1,a"   :one true)
+   (test 5             5       :one true)
+   (test "a"           "a"     :one true)])
+
+(def-test normalize-text
+  [(test "123
+          456
+          789" "123456789" :one true)
+   (test ""    ""          :one true)
+   (test ["Hello,
+           howdy?" :delimiter " "] "Hello, howdy?" )])
+
+(def-test align-by
+  [(test ["|" "first|" "second|"]          ["first |"
+                                            "second|"])
+   (test [" " "first " "second "]          ["first  "
+                                            "second "])
+   (test ["|" "first|" "second|" "third|"] ["first |"
+                                            "second|"
+                                            "third |"])
+  (test ["|" "first|one" "second|one"]     ["first |one"
+                                            "second|one"])
+  (test ["|" "|first" " |second"]          [" |first"
+                                            " |second"])])
