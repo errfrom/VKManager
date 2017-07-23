35736ffc3b6bfc7910786db43dc0a0d3bde9e7ee
Ivanov Dmitry <errfrom@yandex.ru>
2017-07-24 01:54:28 +0300

    safe program termination was implemented & 'collect' logic was fully described

--- src/Collect.hs
+++ src/Collect.hs
@@ -1,21 +1,31 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Collect
-       (CollectOptions(..)
-       ,collect) where
+       ( CollectOptions(..)
+       , collect ) where
 
-import qualified Parser.Receiver as Receiver
-import qualified Parser.Handler  as Handler
-import qualified DBInteract      as DB
+import qualified Parser.Receiver          as Receiver
+import qualified Parser.Handler           as Handler
+import qualified Internal.Utils           as Utils     (cleanAndPrint)
 import           Types.DataBase
-import qualified Data.List       as List (nub)
-import qualified Data.Maybe      as M    (catMaybes, fromJust)
+-- DataBase interaction
+import qualified DBInteract               as DB
+import qualified Database.SQLite.Simple   as SQLite    (Connection, execute
+                                                       ,close)
+import           Database.SQLite.Simple
+-- Data modules
+import qualified Data.Text                as Text      (pack)
+import qualified Data.List                as List      (nub)
+import qualified Data.Maybe               as M         (catMaybes, maybe)
+import qualified Data.ByteString.Lazy     as LBS       (ByteString(..))
+import qualified Data.IORef               as IORef     (IORef(..), newIORef, readIORef
+                                                       ,modifyIORef')
 
-data CollectOptions =
-  CollectOpts { pathDB      :: String
-              , accessToken :: String
-              , startUID    :: Int
-              , maxRecords  :: Int }
+data CollectOptions = CollectOpts
+  { pathDB      :: String
+  , accessToken :: String
+  , startUID    :: Int
+  , maxRecords  :: Int }
 
 -- | Оставляет только те наборы данных,
 -- значение user которых представляет
@@ -23,7 +33,7 @@ data CollectOptions =
 removeInvalidDataChunks :: [DataChunk Int] -> [DataChunk Int]
 removeInvalidDataChunks [] = []
 removeInvalidDataChunks (dc@(DataChunk{..}):dataChunks) =
-  case user of
+  case dcUser of
     Deleted -> checkNext
     Banned  -> checkNext
     _       -> dc : checkNext
@@ -34,24 +44,24 @@ removeInvalidDataChunks (dc@(DataChunk{..}):dataChunks) =
 -- возвращает множество значений требуемого поля.
 getUserFieldsFromDataChunks :: [DataChunk Int] -> (User Int -> Maybe Int) -> [Int]
 getUserFieldsFromDataChunks dataChunks field = (List.nub . M.catMaybes . worker) dataChunks
-  where worker (DataChunk{..}:chunks) = (field user) : worker chunks
+  where worker (DataChunk{..}:chunks) = (field dcUser) : worker chunks
         worker [] = []
 
 getCityIds :: [DataChunk Int] -> [Int]
-getCityIds chunks = getUserFieldsFromDataChunks chunks city
+getCityIds chunks = getUserFieldsFromDataChunks chunks userCity
 
 getCountryIds :: [DataChunk Int] -> [Int]
-getCountryIds chunks = getUserFieldsFromDataChunks chunks country
+getCountryIds chunks = getUserFieldsFromDataChunks chunks userCountry
 
 -- | Обновляет значения city и country поля user в каждом валидном
 -- наборе данных. Заменяет идентификаторы на названия, т.е.
 -- числовые значения на строковые.
 updateChunks :: [DataChunk Int] -> [City] -> [Country] -> [DataChunk String]
 updateChunks [] _ _ = []
-updateChunks (dc@(DataChunk{user = u@User{..}}):chunks) cities countries =
-  let cityName    = getCityNameById city cities
-      countryName = getCountryNameById country countries
-      updatedDC   = dc {user = u {city = cityName, country = countryName}}
+updateChunks (dc@(DataChunk{dcUser = u@User{..}}):chunks) cities countries =
+  let cityName    = getCityNameById userCity cities
+      countryName = getCountryNameById userCountry countries
+      updatedDC   = dc {dcUser = u {userCity = cityName, userCountry = countryName}}
   in updatedDC : (updateChunks chunks cities countries)
   where getCityNameById Nothing _ = Nothing
         getCityNameById _ []      = Nothing
@@ -65,17 +75,67 @@ updateChunks (dc@(DataChunk{user = u@User{..}}):chunks) cities countries =
          |cid == countryId = Just countryName
          |otherwise        = getCountryNameById v countries
 
-writeToDB :: DataChunk String -> IO()
-writeToDB dataChunk = print "TODO + обновление счетчика"
+-- TODO: refactor
+-- | Подготавливает набор данных для записи,
+-- создавая промежуточные экземпляры типов
+-- и, непосредственно, осуществляет запись в базу данных.
+-- По окончанию записи, обновляет счетчик.
+writeToDB :: (Enum a, Show a) => SQLite.Connection -> IORef.IORef a -> DataChunk String -> IO()
+writeToDB conn counterRef DataChunk{..} =
+  let usersQuery      = DB.buildInsertQuery "USERS"         10
+      schoolQuery     = DB.buildInsertQuery "SCHOOLS"       2
+      univerQuery     = DB.buildInsertQuery "UNIVERSITIES"  2
+      schoolConnQuery = DB.buildInsertQuery "USERS_SCHOOLS" 2
+      univerConnQuery = DB.buildInsertQuery "USERS_UNIVERS" 2
+      userSNConnQuery = DB.buildInsertQuery "USERS_SNS"     3
+      uid             = userId dcUser
+      univerConn = UniversityConnect uid <$> (universityId <$> dcUniver)
+      schoolConn = case dcSchools of
+                     Nothing  -> Nothing
+                     (Just a) -> Just $ map (SchoolConnect uid . schoolId) a
+      snConn     = case dcSNetworks of
+                     Nothing -> Nothing
+                     (Just a) -> Just $ map (SNetworkConnect uid) a
+  in do
+    DB.failoverExecute conn usersQuery                       dcUser
+    maybeDoNothing (executeMany conn schoolQuery)            dcSchools
+    maybeDoNothing (DB.failoverExecute conn univerQuery)     dcUniver
+    maybeDoNothing (DB.failoverExecute conn univerConnQuery) univerConn
+    maybeDoNothing (executeMany conn schoolConnQuery)        schoolConn
+    maybeDoNothing (executeMany conn userSNConnQuery)        snConn
+    updateCounter
+  where updateCounter  = counter counterRef
+        maybeDoNothing = M.maybe (return())
+        executeMany conn query a = mapM_ (DB.failoverExecute conn query) a
 
+getValidChunks :: Maybe (LBS.ByteString) -> Maybe [DataChunk Int]
+getValidChunks mRespUsers = do
+  respUsers   <- mRespUsers
+  mixedChunks <- Handler.handleChunks respUsers
+  return (removeInvalidDataChunks mixedChunks)
+
+-- | Простой счетчик, при каждом вызове обновляющий переменную.
+counter :: (Enum a, Show a) => IORef.IORef a -> IO ()
+counter a = do
+  IORef.modifyIORef' a succ
+  value <- IORef.readIORef a
+  let outMsg = "Обработано записей: " ++ (show value)
+  Utils.cleanAndPrint outMsg False
+
+-- | Является точкой входа команды 'collect'.
 collect :: CollectOptions -> IO()
-collect opts@CollectOpts{..} =
-  let limit         = startUID + maxRecords
-      queryQuantity = 500
-      ranges        = getRanges queryQuantity startUID limit
-  --in map (worker opts) ranges
-  in print "work"
-  where worker opts@CollectOpts{..} range = do
+collect opts@CollectOpts{..} = do
+  conn  <- DB.initDB pathDB
+  start <- if (startUID == 0)
+             then (DB.getStartValue conn)
+             else (return startUID)
+  cRef  <- IORef.newIORef 0
+  let query  = 500
+      limit  = start + maxRecords
+      ranges = getRanges query start limit
+  mapM_ (worker cRef conn accessToken) ranges
+  putStrLn "\nГотово."
+  where worker ref conn accessToken range = do
           -- ^ Порядок выполнения:
           -- 1. Получение JSON представлений пользователей.
           -- 2. Формирование необработанных наборов данных.
@@ -84,27 +144,25 @@ collect opts@CollectOpts{..} =
           -- 5. Получение названий по идентификаторам.
           -- 6. Обновление наборов данных.
           -- 7. Передача управления функции записи в базу.
-          respUsers <- Receiver.receive Receiver.usersURL range accessToken
+          respUsers   <- Receiver.receiveUsers accessToken range
 
-          let validChunks = M.fromJust $ do -- TODO: Избавиться от fromJust.
-                                            --       Может быть причиной многих ошибок.
-                respUsers'  <- respUsers
-                mixedChunks <- Handler.handleChunks respUsers'
-                return (removeInvalidDataChunks mixedChunks)
-              idCities    = getCityIds validChunks
-              idCountries = getCountryIds validChunks
+          let mValidChunks = getValidChunks respUsers
+              idCities    = M.maybe [] getCityIds    mValidChunks
+              idCountries = M.maybe [] getCountryIds mValidChunks
 
-          respCities    <- Receiver.receive Receiver.citiesURL idCities accessToken
-          respCountries <- Receiver.receive Receiver.countriesURL idCountries accessToken
+          mRespCities    <- Receiver.receiveCities    accessToken idCities
+          mRespCountries <- Receiver.receiveCountries accessToken idCountries
 
-          let updatedChunks = M.fromJust $ do
-                respCities'    <- respCities
-                respCountries' <- respCountries
-                cities         <- (Handler.handleViaFun Handler.parseCity)    respCities'
-                countries      <- (Handler.handleViaFun Handler.parseCountry) respCountries'
-                return (updateChunks validChunks cities countries)
-          -- Запись в базу
-          mapM_ writeToDB updatedChunks
+          let updatedChunks = do
+               respCities    <- mRespCities
+               respCountries <- mRespCountries
+               validChunks   <- mValidChunks
+               cities        <- Handler.handleCities respCities
+               countries     <- Handler.handleCountries respCountries
+               return (updateChunks validChunks cities countries)
+
+          M.maybe (putStrLn "Возникла ошибка во время выполнения.")
+                  (mapM_ (writeToDB conn ref)) updatedChunks
 
         getRanges :: (Enum a, Num a, Ord a) => a -> a -> a -> [[a]]
         getRanges oneRange start limit =
--- src/DBInteract.hs
+++ src/DBInteract.hs
@@ -6,27 +6,39 @@ module DBInteract
        ( initDB
        , getStartValue
        , buildInsertQuery
-       , failoverInsert ) where
+       , failoverExecute
+       , setKeyboardInterruptHandler ) where
 
 import           Types.DataBase
-import qualified Internal.Utils         as Utils  (endsWith, join
-                                                  ,cleanAndPrint)
-import qualified System.IO              as IO     (FilePath, readFile)
-import qualified System.FilePath.Posix  as Posix  (pathSeparator)
-import qualified System.Directory       as Dir    (doesFileExist
-                                                  ,doesDirectoryExist
-                                                  ,getCurrentDirectory)
-import qualified Data.Text              as Text   (Text(..), pack)
-import qualified Data.Int               as Int    (Int64)
-import qualified Data.List.Split        as Split  (endBy)
-import qualified Database.SQLite.Simple as SQLite (ResultError(..), Connection
-                                                  ,open, close, execute_
-                                                  ,execute, query_)
-import           Database.SQLite.Simple           (ToRow(..), SQLData(..))
-import           Database.SQLite.Simple.Types     (Query(..))
-import           Database.SQLite3                 (SQLError(..), Error(..))
-import           Control.Exception                (catch)
-import           Text.Printf                      (printf)
+import qualified Internal.Utils         as Utils   (endsWith, join
+                                                   ,cleanAndPrint)
+import qualified System.IO              as IO      (FilePath, readFile)
+import qualified System.Exit            as Exit    (ExitCode(..))
+import qualified System.FilePath.Posix  as Posix   (pathSeparator)
+import qualified System.Directory       as Dir     (doesFileExist
+                                                   ,doesDirectoryExist
+                                                   ,getCurrentDirectory)
+import qualified Data.Text              as Text    (Text(..), pack)
+import qualified Data.Int               as Int     (Int64)
+import qualified Data.List.Split        as Split   (endBy)
+import qualified Database.SQLite.Simple as SQLite  (ResultError(..), Statement(..)
+                                                   ,Connection
+                                                   ,open, close
+                                                   ,closeStatement, bind
+                                                   ,withStatement, execute_
+                                                   ,execute, query_)
+import qualified Control.Exception      as Exc     (throwTo)
+import qualified Control.Concurrent     as Conc    (myThreadId)
+import           Control.Monad                     (void)
+import qualified System.Posix.Signals   as Signals (Handler(..)
+                                                   ,installHandler
+                                                   ,keyboardSignal)
+import           Database.SQLite.Simple            (ToRow(..), SQLData(..))
+import           Database.SQLite.Simple.Types      (Query(..))
+import qualified Database.SQLite3       as Base    (step)
+import           Database.SQLite3                  (SQLError(..), Error(..))
+import           Control.Exception                 (catch, handle)
+import           Text.Printf                       (printf)
 
 -- Basic functional
 -----------------------------------------------------------------------------
@@ -90,15 +102,44 @@ buildInsertQuery tableName numColumns =
       columnSymbols = Utils.join "," ["?" | _ <- [1..numColumns]] :: String
   in (Query . Text.pack) $ printf insertPattern tableName columnSymbols
 
-failoverInsert :: (ToRow t) => SQLite.Connection -> Query -> t -> IO ()
-failoverInsert conn query obj =
-  (SQLite.execute conn query obj) `catch` checkErrors
-  where doNothing = return ()
+failoverExecute :: (ToRow t) => SQLite.Connection -> Query -> t -> IO ()
+failoverExecute conn query params =
+   withStatementParams conn query params $ \statmt@(SQLite.Statement baseStatmt) -> do
+    setKeyboardInterruptHandler conn statmt
+    checkErrors `handle` (void . Base.step $ baseStatmt)
+  where withStatementParams :: (ToRow params)
+                               => SQLite.Connection
+                               -> Query
+                               -> params
+                               -> (SQLite.Statement -> IO a)
+                               -> IO a
+        withStatementParams conn query params action =
+          SQLite.withStatement conn query $ \statmt ->
+            SQLite.bind statmt (toRow params) >> action statmt
+
         checkErrors :: SQLError -> IO ()
-        checkErrors (SQLError ErrorConstraint _ _) = doNothing
+        checkErrors (SQLError ErrorConstraint _ _) = return ()
         checkErrors _ = do
-          Utils.cleanAndPrint "Ошибка записи в базу. Завершение работы..."
+          Utils.cleanAndPrint "Ошибка записи в базу. Завершение работы..." True
           SQLite.close conn
+
+-- NOTE: При безопасном завершении, возникает 'Segmentation fault: 11'.
+--       Однако, не замечено связанных с этим проблем.
+-- Обрабатывает завершение нажатием сочетиния клавиш 'Ctrl+C'.
+-- Закрывает последний statement и соединение с базой данных, тем самым
+-- обеспечивая сохранность данных при импровизированном завершении.
+setKeyboardInterruptHandler :: SQLite.Connection -> SQLite.Statement
+                                                 -> IO Signals.Handler
+setKeyboardInterruptHandler conn statmt =
+  let catchExit = Signals.Catch (safeExit conn statmt)
+  in Signals.installHandler Signals.keyboardSignal catchExit Nothing
+  where terminatedCode = 130
+        safeExit conn statmt  = do
+          putStrLn "Безопасное завершение..."
+          SQLite.closeStatement statmt
+          SQLite.close conn
+          tid <- Conc.myThreadId
+          Exc.throwTo tid (Exit.ExitFailure terminatedCode)
 
 -- ToRow instances
 -----------------------------------------------------------------------------
